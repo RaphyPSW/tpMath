9ebd6269c0e8e2ba84a3c31969db28128da82d24
KuuChara <51326998+KuuChara@users.noreply.github.com>
2019-11-22 19:37:57 -0500

    yeet
    aaaahhhhh shit

---
+++ TP MATH Phil Raph/Assets/Scripts/Bombes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombes : MonoBehaviour
+{
+    public float rayon;
+    public float originalForce;
+    const float e = 2.7183f;
+    public int courbeDistri;
+    List<int> buildinglist;
+    List<int> partlist;
+
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
